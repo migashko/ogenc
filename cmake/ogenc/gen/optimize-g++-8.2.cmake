55e22d8f4943bb1d3cbef82a324f3a615a645f09
Vladimir Migashko <migashko@gmail.com>
2021-04-22 01:31:40 +0300

    up

--- cmake/ogenc/gen/optimize-g++-8.2.cmake
+++ cmake/ogenc/gen/optimize-g++-8.2.cmake
@@ -1,4 +1,3 @@
-ogenc_optimize(-fstack-clash-protection "Insert code to probe each page of stack space as it is allocated to protect from stack-clash style attacks." OFF)
 ogenc_optimize(-fwrapv-pointer "Assume pointer overflow wraps around." OFF)
 
 
